8df9b85eca51f4b8b75d060eef0b18a5ef30f326
Zack Pollard <iam@zackpollard.pro>
2015-07-23 05:12:39 +0100

    Implemented offline login.

--- src/pro/zackpollard/bungeeutil/utils/Utils.java
+++ src/pro/zackpollard/bungeeutil/utils/Utils.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.regex.Pattern;
 
 /**
@@ -158,6 +159,8 @@ public class Utils {
         try {
 
             URL url = new URL(urlString);
+            URLConnection hc = url.openConnection();
+            hc.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
             reader = new BufferedReader(new InputStreamReader(url.openStream()));
             StringBuffer buffer = new StringBuffer();
 
